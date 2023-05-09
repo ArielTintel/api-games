bc6b11e5677aaeb6d597a7a6c15c90225da54172
ArielTintel <arielandrade97@gmail.com>
2023-05-09 13:12:59 -0300

    Project created

---
+++ src/test/java/br/com/arieltintel/apigames/ApiGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arieltintel.apigames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
